9e7e09af7e43ece92754bec9b2375ef3b03450c0
LeadNess <leadness@mail.ru>
2020-04-10 00:52:22 +0300

    Upgrade models.py
    Fixed bugs

--- shopdb/models.py
+++ shopdb/models.py
@@ -6,8 +6,8 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True, nullable=False)
     password_hash = db.Column(db.String(128), nullable=False)
-    workers = db.relationship('Worker', backref='user', uselist=False)
     customers = db.relationship('Customer', backref='user', uselist=False)
+    workers = db.relationship('Worker', backref='user', uselist=False)
     suppliers = db.relationship('Supplier', backref='user', uselist=False)
 
     def __repr__(self):
@@ -18,7 +18,7 @@ class Product(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(64), index=True, unique=True, nullable=False)
     warehouses = db.relationship('Warehouse', backref='product', uselist=False)
-    suppliers = db.relationship('Worker', backref='product')
+    suppliers = db.relationship('Supplier', backref='product')
     orders = db.relationship('Order', backref='product')
     deliveries = db.relationship('Delivery', backref='product')
 
@@ -40,23 +40,23 @@ class Address(db.Model):
 
 class Customer(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), index=True, unique=True, nullable=False)
-    address_id = db.Column(db.Integer, db.ForeignKey('addresses.id'), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), index=True, unique=True, nullable=False)
+    address_id = db.Column(db.Integer, db.ForeignKey('address.id'), nullable=False)
     first_name = db.Column(db.String(16), nullable=False)
     second_name = db.Column(db.String(16), nullable=False)
-    orders = db.relationship('Order', backref='address')
+    orders = db.relationship('Order', backref='customer')
 
     def __repr__(self):
-        return '<Worker: {} {}>'.format(self.first_name, self.second_name)
+        return '<Customer: {} {}>'.format(self.first_name, self.second_name)
 
 
 class Supplier(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
-    product_id = db.Column(db.Integer, db.ForeignKey('products.id'), nullable=False)
-    address_id = db.Column(db.Integer, db.ForeignKey('addresses.id'), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    product_id = db.Column(db.Integer, db.ForeignKey('product.id'), nullable=False)
+    address_id = db.Column(db.Integer, db.ForeignKey('address.id'), nullable=False)
     name = db.Column(db.String(128))
-    deliveries = db.relationship('Delivery', backref='address')
+    deliveries = db.relationship('Delivery', backref='supplier')
 
     def __repr__(self):
         return '<Supplier: {}(product_id={})>'.format(self.name, self.product_id)
@@ -64,7 +64,7 @@ class Supplier(db.Model):
 
 class Worker(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), unique=True, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), unique=True, nullable=False)
     first_name = db.Column(db.String(16), nullable=False)
     second_name = db.Column(db.String(16), nullable=False)
     salary = db.Column(db.Integer)
@@ -76,7 +76,7 @@ class Worker(db.Model):
 
 class Warehouse(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    product_id = db.Column(db.Integer, db.ForeignKey('products.id'), unique=True, nullable=False)
+    product_id = db.Column(db.Integer, db.ForeignKey('product.id'), unique=True, nullable=False)
     quantity = db.Column(db.Integer, nullable=False)
 
     def __repr__(self):
@@ -85,8 +85,8 @@ class Warehouse(db.Model):
 
 class Order(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    product_id = db.Column(db.Integer, db.ForeignKey('products.id'), nullable=False)
-    customer_id = db.Column(db.Integer, db.ForeignKey('customers.id'), nullable=False)
+    product_id = db.Column(db.Integer, db.ForeignKey('product.id'), nullable=False)
+    customer_id = db.Column(db.Integer, db.ForeignKey('customer.id'), nullable=False)
     quantity = db.Column(db.Integer, nullable=False)
     cost = db.Column(db.Integer, nullable=False)
     date = db.Column(db.DateTime, index=True, default=datetime.utcnow)
@@ -97,8 +97,8 @@ class Order(db.Model):
 
 class Delivery(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    product_id = db.Column(db.Integer, db.ForeignKey('products.id'), nullable=False)
-    supplier_id = db.Column(db.Integer, db.ForeignKey('suppliers.id'), nullable=False)
+    product_id = db.Column(db.Integer, db.ForeignKey('product.id'), nullable=False)
+    supplier_id = db.Column(db.Integer, db.ForeignKey('supplier.id'), nullable=False)
     quantity = db.Column(db.Integer, nullable=False)
     cost = db.Column(db.Integer, nullable=False)
     date = db.Column(db.DateTime, index=True, default=datetime.utcnow)
